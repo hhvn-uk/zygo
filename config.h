8726efe4673acb05acbf3bb8cac871f32bca7397
hhvn <dev@hhvn.uk>
2022-01-16 19:42:08 +0000

    config.h: comment a bit

--- config.h
+++ config.h
@@ -10,22 +10,27 @@ static short eid_pair[2] = {4,   -1};
 static int stimeout = 5;
 
 static Scheme scheme[] = {
-	{'i',	"    ",	-1 },
-	{'0',	"Text",	-1 },
-	{'1',	"Dir ",	-1 },
-	{'2',	"CCSO",	-1 },
-	{'4',	"Bin ",	-1 },
-	{'5',	"Bin ",	-1 },
-	{'9',	"Bin ",	-1 },
-	{'7',	"Srch",	-1 },
-	{'8',	"Teln",	-1 },
-	{'T',	"Teln",	-1 },
-	{'+',	"Alt ",	-1 },
-	{'I',	"Img ",	-1 },
-	{'g',	"Img ",	-1 },
-	{'h',	"HTML",	-1 },
-	{'s',	"Snd ",	-1 },
-	{'d',	"Doc ",	-1 },
-	{'3',	"ERR ", -1 },
-	{'\0',	NULL,	-1, 0 },
+	/* type, name,   fg */
+	{'i',    "    ", -1 },
+	{'0',    "Text", -1 },
+	{'1',    "Dir ", -1 },
+	{'2',    "CCSO", -1 },
+	{'4',    "Bin ", -1 },
+	{'5',    "Bin ", -1 },
+	{'9',    "Bin ", -1 },
+	{'7',    "Srch", -1 },
+	{'8',    "Teln", -1 },
+	{'T',    "Teln", -1 },
+	{'+',    "Alt ", -1 },
+	{'I',    "Img ", -1 },
+	{'g',    "Img ", -1 },
+	{'h',    "HTML", -1 },
+	{'s',    "Snd ", -1 },
+	{'d',    "Doc ", -1 },
+	{'3',    "ERR ", -1 },
+
+	/* DNE! These values are actually used:
+	 * -1 = default foregrounds
+	 *  0 = default colour pair */
+	{'\0',   NULL,	 -1, 0 },
 };
